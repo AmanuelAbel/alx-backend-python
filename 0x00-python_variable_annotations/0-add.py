eb4936a53707302cb24b549c25523228b12f0360
= <=>
2023-01-08 14:18:01 +0300

    committed

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Adds two float numbers
+and returns float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    a Type annotated function that takes
+    two float numbers and returns float
+    number of their sum
+    """
+    return a + b
