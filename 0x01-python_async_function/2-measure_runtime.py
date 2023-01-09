15c4598b973c5ff654282208a85097f11f8add4e
= <=>
2023-01-09 21:03:01 +0300

    committed

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Measure the runtime
+return a float
+"""
+from time import perf_counter
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """ Measures the time """
+    start = perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    finish = perf_counter()
+    return finish - start
